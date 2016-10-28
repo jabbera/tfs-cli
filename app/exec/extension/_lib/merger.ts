jabbera/tfs-cli

commit 4c9b826cad660c209a83a20d4bea5e0d3fd1bd5d

Add an option for specifying multiple individual manifests.

app/exec/extension/_lib/merger.ts
@@ -39,23 +39,34 @@ export class Merger {
 	private gatherManifests(): Q.Promise<string[]> {
 		trace.debug('merger.gatherManifests');
 
-		const globs = this.settings.manifestGlobs.map(p => path.isAbsolute(p) ? p : path.join(this.settings.root, p));
+		if (this.settings.manifestGlobs && this.settings.manifestGlobs.length > 0) {
+			const globs = this.settings.manifestGlobs.map(p => path.isAbsolute(p) ? p : path.join(this.settings.root, p));
 
-		trace.debug('merger.gatherManifestsFromGlob');
-		const promises = globs.map(pattern => Q.nfcall<string[]>(glob, pattern));
+			trace.debug('merger.gatherManifestsFromGlob');
+			const promises = globs.map(pattern => Q.nfcall<string[]>(glob, pattern));
 
-		return Q.all(promises)
-			.then(results => _.unique(_.flatten<string>(results)))
-			.then(results => {
-				if (results.length > 0) {
-					trace.debug("Merging %s manifests from the following paths: ", results.length.toString());
-					results.forEach(path => trace.debug(path));
-				} else {
-					throw new Error("No manifests found from the following glob patterns: \n" + this.settings.manifestGlobs.join("\n"));
-				}
+			return Q.all(promises)
+				.then(results => _.unique(_.flatten<string>(results)))
+				.then(results => {
+					if (results.length > 0) {
+						trace.debug("Merging %s manifests from the following paths: ", results.length.toString());
+						results.forEach(path => trace.debug(path));
+					} else {
+						throw new Error("No manifests found from the following glob patterns: \n" + this.settings.manifestGlobs.join("\n"));
+					}
 
-				return results;
-			});
+					return results;
+				});
+		} else {
+			const manifests = this.settings.manifests
+			if (!manifests || manifests.length === 0) {
+				return Q.reject<string[]>("No manifests specified.");
+			}
+			this.settings.manifests = _.unique(manifests).map(m => path.resolve(m));
+			trace.debug("Merging %s manifest%s from the following paths: ", manifests.length.toString(), manifests.length === 1 ? "" : "s");
+			manifests.forEach(path => trace.debug(path));
+			return Q.resolve(this.settings.manifests);
+		}
 	}
 
 	/**

app/exec/extension/_lib/interfaces.ts
@@ -130,6 +130,11 @@ export interface MergeSettings {
 	 */
 	root: string;
 
+	/*
+	 * List of paths to manifest files
+	 */
+	manifests: string[];
+
 	/**
 	 * List of globs for searching for partial manifests
 	 */

app/exec/extension/create.ts
@@ -44,7 +44,7 @@ export class ExtensionCreate extends extBase.ExtensionBase<CreationResult> {
 	}
 
 	protected getHelpArgs(): string[] {
-		return ["root", "manifestGlobs", "override", "overridesFile", "bypassValidation", "publisher", "extensionId", "outputPath", "locRoot"];
+		return ["root", "manifests", "manifestGlobs", "override", "overridesFile", "bypassValidation", "publisher", "extensionId", "outputPath", "locRoot"];
 	}
 
 	public exec(): Q.Promise<CreationResult> {

app/exec/extension/default.ts
@@ -19,6 +19,7 @@ export class ManifestJsonArgument extends args.JsonArgument<any> {}
 export interface ExtensionArguments extends CoreArguments {
 	extensionId: args.StringArgument;
 	publisher: args.StringArgument;
+	manifests: args.ArrayArgument;
 	manifestGlobs: args.ArrayArgument;
 	outputPath: args.StringArgument;
 	override: ManifestJsonArgument;
@@ -49,12 +50,13 @@ export class ExtensionBase<T> extends TfCommand<ExtensionArguments, T> {
 		this.registerCommandArgument("extensionId", "Extension ID", "Use this as the extension ID instead of what is specified in the manifest.", args.StringArgument);
 		this.registerCommandArgument("publisher", "Publisher name", "Use this as the publisher ID instead of what is specified in the manifest.", args.StringArgument);
 		this.registerCommandArgument("serviceUrl", "Market URL", "URL to the VSS Marketplace.", args.StringArgument, "https://marketplace.visualstudio.com");
-		this.registerCommandArgument("manifestGlobs", "Manifest globs", "List of globs to find manifests.", args.ArrayArgument, "vss-extension.json");
+		this.registerCommandArgument("manifests", "Manifests", "List of individual manifest files (space separated).", args.ArrayArgument, "vss-extension.json");
+		this.registerCommandArgument("manifestGlobs", "Manifest globs", "List of globs to find manifests (space separated).", args.ArrayArgument, null);
 		this.registerCommandArgument("outputPath", "Output path", "Path to write the VSIX.", args.StringArgument, "{auto}");
 		this.registerCommandArgument("override", "Overrides JSON", "JSON string which is merged into the manifests, overriding any values.", ManifestJsonArgument, "{}");
 		this.registerCommandArgument("overridesFile", "Overrides JSON file", "Path to a JSON file with overrides. This partial manifest will always take precedence over any values in the manifests.", args.ReadableFilePathsArgument, null);
-		this.registerCommandArgument("shareWith", "Share with", "List of VSTS Accounts with which to share the extension.", args.ArrayArgument, null);
-		this.registerCommandArgument("unshareWith", "Un-share with", "List of VSTS Accounts with which to un-share the extension.", args.ArrayArgument, null);
+		this.registerCommandArgument("shareWith", "Share with", "List of VSTS Accounts with which to share the extension (space separated).", args.ArrayArgument, null);
+		this.registerCommandArgument("unshareWith", "Un-share with", "List of VSTS Accounts with which to un-share the extension (space separated).", args.ArrayArgument, null);
 		this.registerCommandArgument("vsix", "VSIX path", "Path to an existing VSIX (to publish or query for).", args.ReadableFilePathsArgument);
 		this.registerCommandArgument("bypassValidation", "Bypass local validation", null, args.BooleanArgument, "false");
 		this.registerCommandArgument("locRoot", "Localization root", "Root of localization hierarchy (see README for more info).", args.ExistingDirectoriesArgument, null);
@@ -65,13 +67,14 @@ export class ExtensionBase<T> extends TfCommand<ExtensionArguments, T> {
 	protected getMergeSettings(): Q.Promise<MergeSettings> {
 		return Q.all([
 			this.commandArgs.root.val(),
+			this.commandArgs.manifests.val(),
 			this.commandArgs.manifestGlobs.val(),
 			this.commandArgs.override.val(),
 			this.commandArgs.overridesFile.val(),
 			this.commandArgs.bypassValidation.val(),
 			this.commandArgs.publisher.val(true),
 			this.commandArgs.extensionId.val(true)
-		]).spread<MergeSettings>((root: string[], manifestGlob: string[], override: any, overridesFile: string[], bypassValidation: boolean, publisher: string, extensionId: String) => {
+		]).spread<MergeSettings>((root: string[], manifests: string[], manifestGlob: string[], override: any, overridesFile: string[], bypassValidation: boolean, publisher: string, extensionId: String) => {
 			if (publisher) {
 				_.set(override, "publisher", publisher);
 			}
@@ -100,6 +103,7 @@ export class ExtensionBase<T> extends TfCommand<ExtensionArguments, T> {
 				_.merge(mergedOverrides, contentJSON, override);
 				return {
 					root: root[0],
+					manifests: manifests,
 					manifestGlobs: manifestGlob,
 					overrides: mergedOverrides,
 					bypassValidation: bypassValidation

app/exec/extension/publish.ts
@@ -45,7 +45,7 @@ export class ExtensionPublish extends extBase.ExtensionBase<ExtensionPublishResu
 	protected description = "Publish a Visual Studio Marketplace Extension.";
 
 	protected getHelpArgs(): string[] {
-		return ["root", "manifestGlobs", "override", "overridesFile", "bypassValidation", "publisher", "extensionId", "outputPath", "locRoot",
+		return ["root", "manifests", "manifestGlobs", "override", "overridesFile", "bypassValidation", "publisher", "extensionId", "outputPath", "locRoot",
 			"vsix", "shareWith"];
 	}
 

commit 77ab0936f89e27d390bd38a6c8cb62d23d3cddf3

Respect the root option
The root option was not being respected in specific cases. This was broken in 4c9b826cad660c209a83a20d4bea5e0d3fd1bd5d

app/exec/extension/_lib/merger.ts
@@ -63,7 +63,7 @@ export class Merger {
 			if (!manifests || manifests.length === 0) {
 				return Q.reject<string[]>("No manifests specified.");
 			}
-			this.settings.manifests = _.uniq(manifests).map(m => path.resolve(m));
+            this.settings.manifests = _.uniq(manifests).map(m => path.resolve(path.isAbsolute(m) ? m : path.join(this.settings.root, m)));
 			trace.debug("Merging %s manifest%s from the following paths: ", manifests.length.toString(), manifests.length === 1 ? "" : "s");
 			manifests.forEach(path => trace.debug(path));
 			return Q.resolve(this.settings.manifests);